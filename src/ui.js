466dc97766f453c3f00752ac3ded0c4617ffa19a
manny <rhcplive11@gmail.com>
2026-01-04 09:22:41 -0600

    refactor

--- src/ui.js
+++ src/ui.js
@@ -1,14 +1,43 @@
-import {deleteTodo, getTodos} from "./app";
+import nav from "./nav";
+import Card from "./card";
+
+export default class UiBuilder {
+    #selectedProject;
+    #projects;
+
+    constructor() {
+        this.app = document.getElementById("app");
+        this.#selectedProject = "First Project";
+        console.log("UI Builder Instantiated");
+    }
+
+    #onSelectProject(name) {
+        this.#selectedProject = name;
+        this.render(this.#projects);
+    }
+
+    buildNav(projects) {
+        nav(projects, this.#selectedProject, (name) => this.#onSelectProject(name));
+    }
+
+    buildCards(projects) {
+        const project = projects.find((project) => project.name === this.#selectedProject);
+        console.log(project);
+        const todos = project.todos;
+        todos.forEach(todo => {
+            console.log(todo)
+            this.app.appendChild(Card(todo.getTitle(), todo.getDescription(), todo.getDate(), todo.getPriority(), todo.getCompleted()));
+
+        })
+    }
+
+    render(projects) {
+        this.#projects = projects;
+        this.app.replaceChildren();
+        this.buildNav(projects);
+        this.buildCards(projects);
+    }
+}
 
 
-const todos = getTodos()
 
-todos.forEach(item => {
-    document.createElement('div')
-})
-
-
-const button = document.createElement('button');
-button.addEventListener('click', (e) => {
-    deleteTodo(1)
-})
